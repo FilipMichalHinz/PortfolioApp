0ba47c1972eb2a64185f4a181e1779c0d19b8415
Nilas <nilasmlyer@gmail.com>
2025-05-11 21:29:20 +0200

    watchlist fix and authgaurd for login flow

--- Angular/AppAngular/src/app/app.routes.ts
+++ Angular/AppAngular/src/app/app.routes.ts
@@ -4,25 +4,25 @@ import { PortfolioDetailComponent } from './portfolio-detail/portfolio-detail.co
 import { LoginComponent } from './login/login.component';
 import { WatchlistComponent } from './watchlist/watchlist.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { authGuard } from './auth.guard';
 
 // Define the application's route configuration
 export const routes: Routes = [
   // Route to show a list of all portfolios
-  { path: 'portfolio-list', component: PortfolioListComponent },
+  { path: 'portfolio-list', component: PortfolioListComponent, canActivate: [authGuard] },
 
   // Default route: redirect to dashboard if no specific path is provided
-  {path: 'dashboard', component: DashboardComponent},
+  {path: 'dashboard', component: DashboardComponent, canActivate: [authGuard]},
+
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
 
   // Dynamic route: shows details for a specific portfolio by its ID
-  { path: 'portfolio/:id', component: PortfolioDetailComponent },
+  { path: 'portfolio/:id', component: PortfolioDetailComponent, canActivate: [authGuard] },
 
   // Route for login screen
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent,  },
 
-  { path: 'watchlist', component: WatchlistComponent}
+  { path: 'watchlist', component: WatchlistComponent, canActivate: [authGuard]}
 
-  // NOTE: A route for PortfolioItemListComponent is commented out — 
-  // uncomment it if you want to add a separate view for listing portfolio items
-  // { path: 'portfolio', component: PortfolioItemListComponent },
+
 ];
